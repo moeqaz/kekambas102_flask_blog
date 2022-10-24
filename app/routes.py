b2c33ff8603dbad2c8094c9ca2010d65476d54f9
moeqaz <usamaqazi98@gmail.com>
2022-10-24 17:14:08 -0400

    templating

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
+from flask import render_template
 from app import app
 
 # Create a route using the @app.route to trigger function based on endpoint
 @app.route('/')
 def index():
-    return 'Hello this is the index route!'
+    user_info = {
+        'username': 'moeqaz',
+        'email': 'usamaqazi@rrr.com'
+    }
+    return render_template('index.html', user=user_info)
 
 
 # Add another route
