6bf75f79fa44c295ae1f7fea35ed64c7b80c3eb2
moeqaz <usamaqazi98@gmail.com>
2022-10-26 12:05:51 -0400

    commit all from yesterday

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,38 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app import app
+from app.forms import SignUpForm
 
-# Create a route using the @app.route to trigger function based on endpoint
+# Create routes for our app
 @app.route('/')
 def index():
     user_info = {
-        'username': 'moeqaz',
-        'email': 'usamaqazi@rrr.com'
+        'username': 'cbale',
+        'email': 'christianb@movies.com'
     }
-    return render_template('index.html', user=user_info)
+    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    return render_template('index.html', user=user_info, colors=colors)
 
 
-# Add another route
 @app.route('/posts')
 def posts():
-    return 'Hi this is Posts'
+    return render_template('posts.html')
+
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        print('Hooray our form submitted!')
+        # Get data from form
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(email, username, password)
+        # Add a new user to the database
+
+        # Flash a success message
+        flash("You have succesffuly signed up!", "success")
+        # Redirect back to home
+        return redirect(url_for('index'))
+
+    return render_template('signup.html', form=form)
